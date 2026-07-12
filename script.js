596fd74377a298a1c071e8018a12be3f28711c0b
DevMohammadAhmedHassan <mohammadahmedhassan5@gmail.com>
2026-07-12 16:58:59 +0500

    JavaScript done

--- script.js
+++ script.js
@@ -5,14 +5,23 @@ let resultEl = document.querySelector(".age-output");
 
 // Logic
 const calculateAge = () => {
+    if (dateEl.value === "") {
+      resultEl.innerHTML = "Please select a date.";
+      return;
+    }
+    
     let userAge = dateEl.value.split("-");
 
+    if (Number(userAge[0]) <= new Date().getFullYear()) {
+        // new Date().getFullYear(); returns the current year
+        let calAge = new Date().getFullYear() - Number(userAge[0]);
+        console.log(calAge);
 
-    // new Date().getFullYear(); returns the current year
-    let calAge = new Date().getFullYear() - Number(userAge[0]);
-    console.log(calAge);
-
-    resultEl.innerHTML = `Your age is ${calAge} years old.`;
+        resultEl.innerHTML = `Your age is ${calAge} years old.`;
+    }
+    else{
+        resultEl.innerHTML = "Please enter a valid year.";
+    }
 }
 
 // event listener
